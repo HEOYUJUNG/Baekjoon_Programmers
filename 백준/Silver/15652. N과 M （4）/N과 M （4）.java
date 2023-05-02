cc09d521924940f0e7f0e5bca5c53b7e60c6b048
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-05-02 22:36:33 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 21912 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] data;
+	static int[] sel;
+	static StringBuilder sb;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		data = new int[N];
+		for (int i = 0; i < N; i++) {
+			data[i] = i + 1;
+		}
+		sel = new int[M];
+		comb(0, 0);
+		System.out.println(sb);
+	}
+
+	private static void comb(int idx, int sidx) {
+		// 기저 조건
+		if (sidx == M) { // M개 선택 완료
+			for (int x : sel) {
+				sb.append(x + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		// 유도 조건
+		for (int i = idx; i < N; i++) {
+			sel[sidx] = data[i];
+			comb(i, sidx + 1);
+		}
+	}
+}

b6dd1b78228d70b4bc8861cccfb7fdcfc959e932
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-06-18 21:27:10 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 752 ms, Memory: 562156 KB -BaekjoonHub

---
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static List<Integer>[] friends;
+	static boolean[] check;
+	static int[] candy;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 거리에 있는 아이들 수
+		int M = Integer.parseInt(st.nextToken()); // 친구 관계 수
+		int K = Integer.parseInt(st.nextToken()); // 우는 아이는 K미만이어야 함
+		st = new StringTokenizer(br.readLine());
+		candy = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			candy[i] = Integer.parseInt(st.nextToken());
+		}
+
+		friends = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			friends[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			friends[a].add(b);
+			friends[b].add(a);
+		}
+
+		// 친구들끼리 그룹 만들기
+		List<Group> groups = new ArrayList<>();
+		check = new boolean[N + 1];
+		for (int i = 1; i <= N; i++) {
+			if (!check[i]) {
+				groups.add(makeGroup(i));
+			}
+		}
+
+		// dp[r][c] = 1~r번째 그룹까지 있을 때, c명의 사탕을 뺏어서 얻을 수 있는 최대 사탕 개수
+		int[][] dp = new int[groups.size() + 1][K];
+		for (int r = 1; r <= groups.size(); r++) {
+			for (int c = 1; c < K; c++) {
+				Group g = groups.get(r - 1);
+				// 1. c번째 그룹 포함하지 않는 경우
+				dp[r][c] = dp[r - 1][c];
+				// 2. c번째 그룹 포함하는 경우
+				if (c >= g.people) {
+					dp[r][c] = Math.max(dp[r][c], dp[r - 1][c - g.people] + g.candy);
+				}
+			}
+		}
+		System.out.println(dp[groups.size()][K - 1]);
+	}
+
+	static class Group {
+		int people, candy;
+
+		public Group(int people, int candy) {
+			this.people = people;
+			this.candy = candy;
+		}
+	}
+
+	static Group makeGroup(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		int pCnt = 0;
+		int cCnt = 0;
+		q.add(x);
+		check[x] = true;
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			pCnt++;
+			cCnt += candy[cur];
+
+			for (int f : friends[cur]) {
+				if (!check[f]) {
+					check[f] = true;
+					q.add(f);
+				}
+			}
+		}
+		return new Group(pCnt, cCnt);
+	}
+}

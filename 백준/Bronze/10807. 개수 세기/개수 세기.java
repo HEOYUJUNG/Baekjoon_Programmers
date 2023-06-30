4bfb3f9d32c1c16501c2bc73e3a2d8505282ba44
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-07-01 02:54:22 +0900

    [Bronze V] Title: 개수 세기, Time: 120 ms, Memory: 12956 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[201];
+		for (int i = 0; i < N; i++) {
+			arr[sc.nextInt() + 100]++;
+		}
+		int v = sc.nextInt();
+		System.out.println(arr[v + 100]);
+	}
+}

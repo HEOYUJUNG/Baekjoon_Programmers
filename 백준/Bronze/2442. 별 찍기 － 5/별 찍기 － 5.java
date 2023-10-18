c6118a633a69d51e61b8999f1a4c29acb0174cb6
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-10-18 17:22:52 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        int L = 2 * N - 1;
+        for(int i = 1; i <= N; i++) {
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(2 * i - 1));
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
